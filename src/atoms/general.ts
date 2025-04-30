32007b6940bbd8ebe78784c13ae9601b4ee8fa09
sadra <sadra.samadzadeh1381@gmail.com>
2025-04-30 16:04:13 +0200

    default state on the content

--- src/atoms/general.ts
+++ src/atoms/general.ts
@@ -5,4 +5,4 @@ export type ViewType = '_hello' | '_about-me' | '_projects' | '_contact-me';
 
 export const mobileAtom = atom(window.matchMedia(`(max-width: ${theme.responsive.media.lg})`).matches);
 export const page = atom<ViewType>('_hello');
-export const contentManager = atom<string>('');
+export const contentManager = atom<string>('about-me');
